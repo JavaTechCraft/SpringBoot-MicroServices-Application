b6369d741241efe5eeb88f60a600c18fa894911b
Java Techcraft <java528.techcraft@gmail.com>
2022-08-05 19:23:37 +0100

    first commit

---
+++ src/main/java/com/techcraft/springboot/microservices/SpringBootMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.techcraft.springboot.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcraft/springboot/microservices/SpringBootMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcraft.springboot.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
